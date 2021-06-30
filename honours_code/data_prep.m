407f957bfb69c5ca47607b61147d3953a0fd522a
Josh King <josh.king@monash.edu>
2021-06-30 15:02:34 +1000

    Remove honours code folder

--- honours_code/data_prep.m
+++
@@ -1,9 +0,0 @@
-function [x, y] = data_prep(filename, worksheet)
-imgdata = xlsread(fullfile('Interferometry data', filename),worksheet);
-numrows = size(imgdata,1);
-numcol = size(imgdata, 2);
-
-x = imgdata(2:numrows,1:1);
-y = imgdata(2: numrows,2:numcol);
-end
- 
