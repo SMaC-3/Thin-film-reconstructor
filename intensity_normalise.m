71ae121240b8d20bfe2a03007f497b8ce04686ba
Josh King <josh.king@monash.edu>
2022-01-07 17:25:17 +1100

    Update to work with data processor, and use global int minimum and maximum

--- intensity_normalise.m
+++ intensity_normalise.m
@@ -1,142 +1,125 @@
-function [norm_int, I_dimp, I_min, I_max] =...
-    intensity_normalise(radial_data, int_data, I_min, I_max)
+function [norm_int, I_dimp] =...
+    intensity_normalise(radial_data, int_data, I_min, I_max, dimple_radius,...
+    int_min, int_max)
 
 I_sp = sort([I_min; I_max]);
 sp = int_data(I_sp);
 
-maxx = max(sp);
-minn = min(sp);
+% maxx = max(sp);
+% minn = min(sp);
+maxx = int_max;
+minn = int_min;
 %--------------------------------------------------------------------------
 % Identify dimple index
 %--------------------------------------------------------------------------
 
-figure(3)
-plot(radial_data, int_data, 'black', 'LineWidth', 2)
-hold on
-scatter(radial_data(I_sp), int_data(I_sp), 200, 'black', 'filled')
+[~, I_radius_dimp] = min(abs(radial_data - dimple_radius));
 
-for k = 1:length(I_sp)
-   
-    text(radial_data(I_sp(k)), mean(int_data) ,num2str(k))
-    
-end
-hold off
-disp([radial_data(I_sp).' ; 1:length(I_sp)])
-
-dimp_prompt = 'Identify index of dimple (enter 0 to manually add a sp or -1 to manually remove a sp): ';
-I_dimp = input(dimp_prompt);
-while isempty(I_dimp)
-    I_dimp = input(dimp_prompt);
-end
-
-while I_dimp == 0 | I_dimp == -1
-    
-    if I_dimp == 0
-        man_ID_prompt = 'please select pixel value for SP: ';
-        man_ID = input(man_ID_prompt);
-        while isempty(man_ID)
-            man_ID = input(man_ID_prompt);
-        end
-        [~, mvI] = min(abs(radial_data - man_ID));
-        
-        man_ID_prompt_2 = 'Is this a max or min [enter 1 or 0]: ';
-        man_ID_2 = input(man_ID_prompt_2);
-        while isempty(man_ID_2)
-            man_ID_2 = input(man_ID_prompt_2);
-        end
-        
-        
-        if man_ID_2 == 1
-            I_max = [I_max;mvI];
-        elseif man_ID_2 == 0
-            I_min = [I_min;mvI];
-        end
-        
-        I_sp = [I_sp; mvI];
-        [I_sp, I_sp_sorted] = sort(I_sp);
-        
-        sp = [sp; int_data(mvI)];
-        sp = sp(I_sp_sorted);
-        
-    
-    elseif I_dimp == -1
-         
-        man_ID_prompt = 'please select pixel value for SP: ';
-        man_ID = input(man_ID_prompt);
-        while isempty(man_ID)
-            man_ID = input(man_ID_prompt);
-        end
-        
-        [~, mvI] = min(abs(radial_data - man_ID));
-        [~,find_I] = min(abs(I_sp-mvI));
-        
-        I_sp(find_I) = [];
-        sp(find_I) = [];
-        
-        is_max = ismember(I_max, I_sp(find_I));
-        I_max(is_max) = [];
-        is_min = ismember(I_min, I_sp(find_I));
-        I_min(is_min) = [];
-    
-    end
-    
-    figure(3)
-    plot(radial_data, int_data, 'black', 'LineWidth', 2)
-    hold on
-    scatter(radial_data(I_sp), int_data(I_sp), 200, 'black', 'filled')
-    for k = 1:length(I_sp)
-    text(radial_data(I_sp(k)), mean(int_data) ,num2str(k))  
-    end
-    hold off
-    disp([radial_data(I_sp).' ; 1:length(I_sp)])
-    
-    dimp_prompt = 'Identify index of dimple (enter 0 to manually add a sp or -1 to manually remove a sp): ';
-    I_dimp = input(dimp_prompt);
-    while isempty(I_dimp)
-        I_dimp = input(dimp_prompt);
-    end
-
-end
-
+I_dimp = find(I_sp == I_radius_dimp);
 if ismember(I_sp(I_dimp),I_min)
     dimp_max_min = -1;
 elseif ismember(I_sp(I_dimp),I_max)
     dimp_max_min = 1;
 end
     
-
 dimp_in = I_dimp-1;
+
+if dimp_in > 0
+    factor = sp(dimp_in+2)/sp(dimp_in);
+else 
+    factor =1;
+end
+int_data(I_sp(I_dimp+1):end)=int_data(I_sp(I_dimp+1):end)/factor;
+sp(I_dimp+1:end) = int_data(I_sp(I_dimp+1:end));
+% dif_sp = I_sp(dimp_in+2) - I_sp(dimp_in+1);
+% correction = linspace(0,factor, dif_sp+1).';
+% int_data(I_sp(I_dimp):I_sp(I_dimp+1)) =...
+%     int_data(I_sp(I_dimp):I_sp(I_dimp+1))./correction;
+% sp(I_dimp+1) = int_data(I_sp(I_dimp+1));
+
+
+if min(I_min) < min(I_max)
+    sp_first = -1;
+else
+    sp_first = 1;
+end 
 
 norm_int = zeros(length(int_data),1);
 
 %--------------------------------------------------------------------------
 
-for i = 1:length(sp)-1
-    if i == 1 % Dimple center not true stationary point, use global max/min? 
-
-        norm_int(1:I_sp(i+1)) =... 
-            (2*int_data(1:I_sp(i+1)) - (min(sp(i:i+1)) + max(sp(i:i+1))) )...
-            ./(max(sp(i:i+1)) - min(sp(i:i+1)));
+for i = 0:length(sp)-1
+    if i==0 && dimp_in == 0 || i==1 && dimp_in == 0
+        if dimp_max_min == -1
+            norm_int(1:I_sp(i+2)) = ...
+                (2*int_data(1:I_sp(i+2)) - (maxx+minn))./(maxx-minn);
+        elseif dimp_max_min == 1
+            norm_int(1:I_sp(i+2)) = ...
+                (2*int_data(1:I_sp(i+2)) - (maxx+minn))./(maxx-minn);
+        end
         
-    elseif i~= 1 && i< dimp_in
-    
+    elseif i == 0 && dimp_in ~= 0 % Dimple center not true stationary point, use global max/min?
+        
+        %         norm_int(1:I_sp(i+1)) =...
+        %             (2*int_data(1:I_sp(i+1)) - (min(sp(i:i+1)) + max(sp(i:i+1))) )...
+        %             ./(max(sp(i:i+1)) - min(sp(i:i+1)));
+        
+        %         norm_int(1:I_sp(i+1)) =...
+        %             (2*int_data(1:I_sp(i+1)) - (minn + maxx) )...
+        %             ./(maxx - minn);
+        
+        if sp_first == -1
+            
+            %         norm_int(1:I_sp(i+1)) =...
+            %             (2*int_data(1:I_sp(i+1)) - (sp(1) + max(int_data)) )...
+            %             ./(max(int_data) - sp(1));
+            
+            norm_int(1:I_sp(i+1)) =...
+                (2*int_data(1:I_sp(i+1)) - (sp(1) + maxx) )...
+                ./(maxx - sp(1));
+            
+            
+        elseif sp_first == 1
+            
+            if min(int_data(1:I_sp(I_dimp))) < minn
+                min_first = min(int_data(1:I_sp(I_dimp)));
+            else
+                min_first = minn;
+            end
+            
+            %             norm_int(1:I_sp(i+1)) =...
+            %                 (2*int_data(1:I_sp(i+1)) - (sp(1) + min_first) )...
+            %                 ./(sp(1) - min_first);
+            norm_int(1:I_sp(i+1)) =...
+                (2*int_data(1:I_sp(i+1)) - (sp(1) + minn) )...
+                ./(sp(1) - minn);
+            
+            %             norm_int(1:I_sp(i+1)) =...
+            %                 (2*int_data(1:I_sp(i+1)) - (maxx + minn) )...
+            %                 ./(maxx - minn);
+        end
+        
+    elseif i~= 0 && i< dimp_in
+        
         norm_int(I_sp(i):I_sp(i+1)) = ...
             (2*int_data(I_sp(i):I_sp(i+1)) - (min(sp(i:i+1)) + max(sp(i:i+1))) )...
             ./(max(sp(i:i+1)) - min(sp(i:i+1)));
-    
-    elseif i == dimp_in || i == dimp_in + 1
         
-%         if dimp_max_min == -1
-%             norm_int(I_sp(i):I_sp(i+1)) = ...
-%                 (2*int_data(I_sp(i):I_sp(i+1)) - (max(sp(i:i+1))+minn))./(max(sp(i:i+1))-minn);
-%         elseif dimp_max_min == 1
-%             norm_int(I_sp(i):I_sp(i+1)) = ...
-%                 (2*int_data(I_sp(i):I_sp(i+1)) - (maxx+min(sp(i:i+1))))./(maxx-min(sp(i:i+1)));
-%         end
-
-        norm_int(I_sp(i):I_sp(i+1)) = ...
-            (2*int_data(I_sp(i):I_sp(i+1)) - (maxx+minn))./(maxx-minn);
-
+    elseif i == dimp_in || i == dimp_in + 1 && dimp_in ~=0
+        
+        
+        
+        if dimp_max_min == -1
+            norm_int(I_sp(i):I_sp(i+1)) = ...
+                (2*int_data(I_sp(i):I_sp(i+1)) - (max(sp(i:i+1))+minn))./(max(sp(i:i+1))-minn);
+        elseif dimp_max_min == 1
+            norm_int(I_sp(i):I_sp(i+1)) = ...
+                (2*int_data(I_sp(i):I_sp(i+1)) - (maxx+min(sp(i:i+1))))./(maxx-min(sp(i:i+1)));
+        end
+        
+        %         norm_int(I_sp(i):I_sp(i+1)) = ...
+        %             (2*int_data(I_sp(i):I_sp(i+1)) - (maxx+minn))./(maxx-minn);
+        
     elseif i>dimp_in+1 && i<length(sp)
         norm_int(I_sp(i):I_sp(i+1)) = ...
             (2*int_data(I_sp(i):I_sp(i+1)) - (min(sp(i:i+1)) + max(sp(i:i+1))) )...
@@ -144,11 +127,17 @@ for i = 1:length(sp)-1
         
     else
         norm_int(I_sp(i):end) =...
-             (2*int_data(I_sp(i):end) -...
-             (min(int_data(I_sp(i):end)) +max(int_data(I_sp(i):end))) )...
-             ./(max(int_data(I_sp(i):end)) - min(int_data(I_sp(i):end)));
+            (2*int_data(I_sp(i):end) -...
+            (min(int_data(I_sp(i):end)) +max(int_data(I_sp(i):end))) )...
+            ./(max(int_data(I_sp(i):end)) - min(int_data(I_sp(i):end)));
         
     end
 end
+
+figure(3)
+plot(radial_data, norm_int, 'black', 'LineWidth', 2)
+hold on
+scatter(radial_data(I_sp), norm_int(I_sp), 200, 'black', 'filled')
+hold off
 
 end
