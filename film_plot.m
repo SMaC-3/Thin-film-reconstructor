b3c2ba9377b4af8cd907d2f7351a950c47aa4ee6
Josh King <josh.king@monash.edu>
2022-01-07 17:08:52 +1100

    Add film metrics plots (volume, rim, min height, shear rate)

--- film_plot.m
+++ film_plot.m
@@ -1,4 +1,4 @@
-function film_plot(T_film_metrics,T_film_plot)
+function film_plot(T_film_metrics,T_film_plot,T_film_shear)
 numFilms = size(T_film_plot,2)-1;
  
 figure(1);
@@ -113,6 +113,77 @@ ylabel('Radius of dimple / \mum','FontWeight','bold');
 
 plot(T_film_metrics.timeStamp,T_film_metrics.Rrim_micron,'-o','LineWidth', 1.5,...
     'Color','blue','MarkerFaceColor','blue', 'MarkerSize',10)
+%--------------------------------------------------------------------------
 
+figure(5);
+hold on
+fig4 = gcf;
+ax4 = gca;
+
+fig4.Color = 'white';
+
+% ax.Units = 'centimeters';
+ax4.LineWidth = 1.5;
+ax4.XColor = 'k';
+ax4.YColor = 'k';
+ax4.FontName = 'Helvetica';
+ax4.FontSize = 18;
+ax4.FontWeight = 'bold';
+ax4.Box= 'off';
+
+
+xlabel('Lateral dimension (\mum)','FontWeight','bold');
+ylabel('Shear rate (1/s)','FontWeight','bold');
+
+
+% Colour blind palette
+pal = {'#000000','#004949','#009292','#ff6db6','#ffb6db','#490092',...
+    '#006ddb','#b66dff','#6db6ff','#b6dbff','#920000',...
+    '#924900','#db6d00','#24ff24','#ffff6d'};
+
+for i = 1:numFilms-1
+    str = pal{i};
+    color = sscanf(str(2:end),'%2x%2x%2x',[1 3])/255;
+    plot([T_film_shear.radius_bar], [T_film_shear{:,i+1}], 'Color',color,...
+        'LineWidth',1.5)
+    
+    % scatter(-rad_blue, blue_int, 50, color,'filled')
+    
+end
+
+%--------------------------------------------------------------------------
+Exist_Column = strcmp('dimpVol_rim',T_film_metrics.Properties.VariableNames);
+val = Exist_Column(Exist_Column==1) ;
+
+if val ==1
+    x_title = 'Cumulative time / s ';
+    
+    figure(6)
+    fig6 = gcf;
+    ax6 = gca;
+    
+    fig6.Color = 'white';
+    
+    % ax.Units = 'centimeters';
+    ax6.LineWidth = 1.5;
+    ax6.XColor = 'k';
+    ax6.YColor = 'k';
+    ax6.FontName = 'Helvetica';
+    ax6.FontSize = 18;
+    ax6.FontWeight = 'bold';
+    ax6.Box= 'off';
+    hold on
+    
+    xlabel(x_title,'FontWeight','bold');
+    ylabel('Dimple volume (\mum^3)','FontWeight','bold');
+    
+    plot(T_film_metrics.timeStamp,T_film_metrics.dimpVol_rim,'-o','LineWidth', 1.5,...
+        'Color','blue','MarkerFaceColor','blue', 'MarkerSize',10)
+    
+    plot(T_film_metrics.timeStamp,T_film_metrics.dimpVol_Hmin,'-o','LineWidth', 1.5,...
+        'Color','cyan','MarkerFaceColor','cyan', 'MarkerSize',10)
+    L = legend();
+    L.String = {'dimple volume @ rim','dimple volume @ minimum height'};
+end
 
 end
