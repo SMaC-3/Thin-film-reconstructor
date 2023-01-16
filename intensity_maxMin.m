32515f7bc5e70b397ed510e9bd0fedf6892c607a
Josh King <josh.king@monash.edu>
2023-01-16 19:03:08 +1100

    Add new processing options.

--- intensity_maxMin.m
+++ intensity_maxMin.m
@@ -1,8 +1,26 @@
-function [I_min_pks, I_max_pks, min_pks, max_pks, cut, cut_grad, dimple_radius] =...
+% Code written by Joshua P. King, SMaCLab, Monash University, Australia
+% Last updated: August, 2022
+
+%   ________  ___      ___       __       ______   ___            __       _______   
+%  /"       )|"  \    /"  |     /""\     /" _  "\ |"  |          /""\     |   _  "\  
+% (:   \___/  \   \  //   |    /    \   (: ( \___)||  |         /    \    (. |_)  :) 
+%  \___  \    /\\  \/.    |   /' /\  \   \/ \     |:  |        /' /\  \   |:     \/  
+%   __/  \\  |: \.        |  //  __'  \  //  \ _   \  |___    //  __'  \  (|  _  \\  
+%  /" \   :) |.  \    /:  | /   /  \\  \(:   _) \ ( \_|:  \  /   /  \\  \ |: |_)  :) 
+% (_______/  |___|\__/|___|(___/    \___)\_______) \_______)(___/    \___)(_______/  
+                                                                                   
+
+% SMaCLab website can be found here:
+% https://sites.google.com/view/smaclab
+
+% -------------------------------------------------------------------------
+
+function [I_min_pks, I_max_pks, min_pks, max_pks, cut, dimple_radius] =...
     intensity_maxMin(radial_data, int_data, key_pars)
 
 % Find peaks
-minPeak = (nanmean(int_data) - nanmin(int_data))*0.1;
+% minPeak = (nanmean(int_data) - nanmin(int_data))*0.1;
+minPeak = (nanmean(int_data) - nanmin(int_data))*0.05;
 [min_pks, I_min_pks] = findpeaks(-int_data,...
     'MinPeakProminence',minPeak);
 min_pks = -min_pks;
@@ -22,30 +40,27 @@ if isempty(key_pars) == 1
     [cutoff_lb] = get_LB_SP_ID();
     % Get upper bound trim
     [cutoff_ub] = get_UB_SP_ID();
-    
-    [cutoff_grad_lb] = get_LB_grad_cor();
-    [cutoff_grad_ub] = get_UB_grad_cor();
+
     [dimple_radius, I_radius] =...
-        get_dimple_radius(radial_data,int_data, I_min_pks, I_max_pks);    
+        get_dimple_radius(radial_data,int_data, I_min_pks, I_max_pks);
 
 else
-    
+
     cutoff_lb = key_pars(1);
     cutoff_ub = key_pars(2);
-    cutoff_grad_lb = key_pars(3);
-    cutoff_grad_ub = key_pars(4);
-    dimple_radius = key_pars(5);
-    
+
+    dimple_radius = key_pars(3);
+
     [~, radius_dimp_I] = min(abs(radial_data - dimple_radius));
-    [min_sub, min_sub_I] = min(abs(I_min_pks-radius_dimp_I));    
+    [min_sub, min_sub_I] = min(abs(I_min_pks-radius_dimp_I));
     [max_sub, max_sub_I] = min(abs(I_max_pks - radius_dimp_I));
-    
+
     if min_sub < max_sub
         I_radius = I_min_pks(min_sub_I);
         dimple_radius = radial_data(I_radius);
     elseif max_sub < min_sub | max_sub == min_sub
         I_radius = I_max_pks(max_sub_I);
-        dimple_radius = radial_data(I_radius);  
+        dimple_radius = radial_data(I_radius);
     end
 end
 
@@ -59,12 +74,7 @@ plot([cutoff_lb, cutoff_lb],[min(int_data), max(int_data)], 'black',...
     'LineWidth', 1)
 plot([cutoff_ub, cutoff_ub],[min(int_data), max(int_data)], 'black',...
     'LineWidth', 1)
-plot([cutoff_grad_lb, cutoff_grad_lb],[min(int_data), max(int_data)], '--',...
-    'Color','black',...
-    'LineWidth', 1)
-plot([cutoff_grad_ub, cutoff_grad_ub],[min(int_data), max(int_data)], '--',...
-    'Color','black',...
-    'LineWidth', 1)
+
 scatter(dimple_radius, int_data(I_radius),250,...
     'MarkerFaceColor', 'green','MarkerEdgeColor','green')
 hold off
@@ -72,11 +82,10 @@ hold off
 disp('1: Accept all parameters')
 disp('2: Adjust lower bound for max/min')
 disp('3: Adjust upper bound for max/min')
-disp('4: Adjust lower bound for gradient correction')
-disp('5: Adjust upper bound for gradient correction')
-disp('6: Change selected dimple rim')
-disp('7: Add stationary point')
-disp('8: Remove stationary point')
+disp('4: Change selected dimple rim')
+disp('5: Add stationary point')
+disp('6: Remove stationary point')
+disp('7: Remove stationary points within range')
 
 action_input = 'Please select an option: ';
 action_select = input(action_input);
@@ -85,7 +94,7 @@ while isempty(action_select)
 end
 
 while action_select~=1
-   
+
     if action_select == 2
         disp(strcat('Current lower bound trim: ', ' ',string(cutoff_lb)));
         [cutoff_lb] = get_LB_SP_ID();
@@ -93,26 +102,21 @@ while action_select~=1
         disp(strcat('Current upper bound trim: ', ' ',string(cutoff_ub)));
         [cutoff_ub] = get_UB_SP_ID();
     elseif action_select == 4
-        disp(strcat('Current lower bound for gradient correction: ', ' ',...
-            string(cutoff_grad_lb)));
-        [cutoff_grad_lb] = get_LB_grad_cor();
-    elseif action_select == 5
-        disp(strcat('Current upper bound for gradient correction: ',' ',...
-            string(cutoff_grad_ub)));
-        [cutoff_grad_ub] = get_UB_grad_cor();
-    elseif action_select == 6
         [dimple_radius, I_radius] =...
             get_dimple_radius(radial_data,int_data, I_min_pks, I_max_pks);
-    elseif action_select == 7
+    elseif action_select == 5
         [I_min_pks, min_pks, I_max_pks, max_pks] =...
             add_stationary_point(radial_data, int_data, I_min_pks, I_max_pks);
-    elseif action_select == 8
+    elseif action_select == 6
         [I_min_pks, min_pks, I_max_pks, max_pks] =...
             remove_stationary_point(radial_data, I_min_pks, min_pks, I_max_pks, max_pks);
+    elseif action_select == 7
+        [I_min_pks, min_pks, I_max_pks, max_pks] =...
+            remove_stationary_point_range_index(radial_data, int_data, I_min_pks, min_pks, I_max_pks, max_pks);
     else
         disp('Please enter a valid selection')
     end
-    
+
     figure(2)
     scatter(radial_data(I_min_pks), min_pks,200, 'magenta', 'filled')
     hold on
@@ -122,16 +126,10 @@ while action_select~=1
         'LineWidth', 1)
     plot([cutoff_ub, cutoff_ub],[min(int_data), max(int_data)], 'black',...
         'LineWidth', 1)
-    plot([cutoff_grad_lb, cutoff_grad_lb],[min(int_data), max(int_data)], '--',...
-        'Color','black',...
-        'LineWidth', 1)
-    plot([cutoff_grad_ub, cutoff_grad_ub],[min(int_data), max(int_data)], '--',...
-        'Color','black',...
-        'LineWidth', 1)
     scatter(dimple_radius, int_data(I_radius),250,...
         'MarkerFaceColor', 'green','MarkerEdgeColor','green')
     hold off
-    
+
     action_input = 'Please select an option: ';
     action_select = input(action_input);
     while isempty(action_select)
@@ -139,7 +137,7 @@ while action_select~=1
     end
 end
 
-% Perfrom upper and lower bound trim
+% Perform upper and lower bound trim
 
 [~,I_cutoff] = min(abs(radial_data - cutoff_lb));
 
@@ -166,7 +164,7 @@ I_max_pks = I_max_pks(max_cut_2);
 % Prepare input parameters for return
 
 cut = [cutoff_lb, cutoff_ub];
-cut_grad = [cutoff_grad_lb, cutoff_grad_ub];
+
 end
 
 function  [cutoff_lb] =...
@@ -178,17 +176,6 @@ while isempty(cutoff_lb)
     cutoff_lb = input(trim_lb);
 end
 
-% % cutoff = 20;
-% [~,I_cutoff] = min(abs(radial_data - cutoff_lb));
-% 
-% min_cut = I_min_pks>I_cutoff;
-% max_cut = I_max_pks>I_cutoff;
-% 
-% min_pks = min_pks(min_cut);
-% I_min_pks = I_min_pks(min_cut);
-% 
-% max_pks = max_pks(max_cut);
-% I_max_pks = I_max_pks(max_cut);
 
 end
 
@@ -197,59 +184,35 @@ function  [cutoff_ub] =...
 
 trim = 'Enter upper bound for max/min peak identification: ';
 cutoff_ub = input(trim);
-    while isempty(cutoff_ub)
-        cutoff_ub = input(trim);
-    end
-% cutoff_2 = 195;
-% 
-% [~,I_cutoff_2] = min(abs(radial_data - cutoff_ub));
-% 
-% min_cut_2 = I_min_pks<I_cutoff_2;
-% max_cut_2 = I_max_pks<I_cutoff_2;
-% 
-% min_pks = min_pks(min_cut_2);
-% I_min_pks = I_min_pks(min_cut_2);
-% 
-% max_pks = max_pks(max_cut_2);
-% I_max_pks = I_max_pks(max_cut_2);
-
+while isempty(cutoff_ub)
+    cutoff_ub = input(trim);
 end
-
-function  [cutoff_grad_lb] =...
-    get_LB_grad_cor()
-
-trim_lb = 'Enter lower bound for gradient correction: ';
-cutoff_grad_lb = input(trim_lb);
-while isempty(cutoff_grad_lb)
-    cutoff_grad_lb = input(trim_lb);
-end
-
-end
-
-function  [cutoff_grad_ub] =...
-    get_UB_grad_cor()
-
-trim = 'Enter upper bound for gradient correction: ';
-cutoff_grad_ub = input(trim);
-    while isempty(cutoff_grad_ub)
-        cutoff_grad_ub = input(trim);
-    end
 
 end
 
 function  [dimple_radius, I_radius] =...
     get_dimple_radius(radial_data,int_data, I_min_pks, I_max_pks)
+
 I_sp = sort([I_min_pks; I_max_pks]);
-% sp = int_data(I_sp);
+
+c = linspace(1,10,length(I_sp));
 
 figure(3)
+colormap lines
 plot(radial_data, int_data, 'black', 'LineWidth', 2)
 hold on
-scatter(radial_data(I_sp), int_data(I_sp), 200, 'black', 'filled')
+scatter(radial_data(I_sp), int_data(I_sp), 200, c, 'filled')
+ax = gca;
+cmap = colormap(ax,lines(length(I_sp)));
+ax.YLim(2) = max(int_data)*1.1;
 
 for k = 1:length(I_sp)
-    text(radial_data(I_sp(k)), mean(int_data) ,num2str(k))   
+        text(radial_data(I_sp(k)), max(int_data)*1.01 + (0.01*max(int_data)*(sin(k))) ,num2str(k),...
+            "Color",cmap(k,1:3))
 end
+
+hold off
+disp([radial_data(I_sp).' ; 1:length(I_sp)])
 
 hold off
 disp([radial_data(I_sp).' ; 1:length(I_sp)])
@@ -290,19 +253,12 @@ end
 min_pks = int_data(I_min_pks);
 max_pks = int_data(I_max_pks);
 
-% I_sp = [I_sp; mvI];
-% [I_sp, I_sp_sorted] = sort(I_sp);
-% 
-% sp = [sp; int_data(mvI)];
-% sp = sp(I_sp_sorted);
-
 end
 
 function  [I_min_pks, min_pks, I_max_pks, max_pks] =...
     remove_stationary_point(radial_data, I_min_pks, min_pks, I_max_pks, max_pks)
 
 I_sp = sort([I_min_pks; I_max_pks]);
-% sp = int_data(I_sp);
 
 man_ID_prompt = 'please select pixel value for SP: ';
 man_ID = input(man_ID_prompt);
@@ -320,7 +276,122 @@ is_min = ismember(I_min_pks, I_sp(find_I));
 I_min_pks(is_min) = [];
 min_pks(is_min) = [];
 
-% I_sp(find_I) = [];
-% sp(find_I) = [];
+end
+
+
+function  [I_min_pks, min_pks, I_max_pks, max_pks] =...
+    remove_stationary_point_range(radial_data, I_min_pks, min_pks, I_max_pks, max_pks)
+
+    lower_ID_prompt = 'please select lower bound pixel value for SP removal: ';
+    lower_ID = input(lower_ID_prompt);
+    while isempty(lower_ID)
+        lower_ID = input(lower_ID_prompt);
+    end
+
+    upper_ID_prompt = 'please select upper bound pixel value for SP removal: ';
+    upper_ID = input(upper_ID_prompt);
+    while isempty(upper_ID)
+        upper_ID = input(upper_ID_prompt);
+    end
+    
+while length(lower_ID) ~= length(upper_ID)
+    disp('Length mismatch between lower and upper bounds')
+    
+    lower_ID_prompt = 'please select lower bound pixel value for SP removal: ';
+    lower_ID = input(lower_ID_prompt);
+    while isempty(lower_ID)
+        lower_ID = input(lower_ID_prompt);
+    end
+
+    upper_ID_prompt = 'please select upper bound pixel value for SP removal: ';
+    upper_ID = input(upper_ID_prompt);
+    while isempty(upper_ID)
+        upper_ID = input(upper_ID_prompt);
+    end
+end
+
+
+for i = 1:length(upper_ID)
+    rad_max = radial_data(I_max_pks);
+    rad_min = radial_data(I_min_pks);
+
+    log_max = rad_max > lower_ID(i) & rad_max < upper_ID(i);
+    log_min = rad_min > lower_ID(i) & rad_min < upper_ID(i);
+
+    I_max_pks(log_max) = [];
+    max_pks(log_max) = [];
+    I_min_pks(log_min) = [];
+    min_pks(log_min) = [];
+end
 
 end
+
+function  [I_min_pks, min_pks, I_max_pks, max_pks] =...
+    remove_stationary_point_range_index(radial_data, int_data, I_min_pks, min_pks, I_max_pks, max_pks)
+
+I_sp = sort([I_min_pks; I_max_pks]);
+
+c = linspace(1,10,length(I_sp));
+
+figure(3)
+colormap lines
+plot(radial_data, int_data, 'black', 'LineWidth', 2)
+hold on
+scatter(radial_data(I_sp), int_data(I_sp), 200, c, 'filled')
+ax = gca;
+cmap = colormap(ax,lines(length(I_sp)));
+ax.YLim(2) = max(int_data)*1.1;
+
+for k = 1:length(I_sp)
+        text(radial_data(I_sp(k)), max(int_data)*1.01 + (0.01*max(int_data)*(sin(k))) ,num2str(k),...
+            "Color",cmap(k,1:3))
+end
+
+hold off
+disp([radial_data(I_sp).' ; 1:length(I_sp)])
+
+
+
+    lower_ID_prompt = 'please select lower bound index value for SP removal: ';
+    lower_ID = input(lower_ID_prompt);
+    while isempty(lower_ID)
+        lower_ID = input(lower_ID_prompt);
+    end
+
+    upper_ID_prompt = 'please select upper bound index value for SP removal: ';
+    upper_ID = input(upper_ID_prompt);
+    while isempty(upper_ID)
+        upper_ID = input(upper_ID_prompt);
+    end
+    
+while length(lower_ID) ~= length(upper_ID)
+    disp('Length mismatch between lower and upper bounds')
+
+    lower_ID = input(lower_ID_prompt);
+    while isempty(lower_ID)
+        lower_ID = input(lower_ID_prompt);
+    end
+
+    upper_ID = input(upper_ID_prompt);
+    while isempty(upper_ID)
+        upper_ID = input(upper_ID_prompt);
+    end
+end
+
+for i = 1:length(upper_ID)
+    rad_max = radial_data(I_max_pks);
+    rad_min = radial_data(I_min_pks);
+    lower_rad = radial_data(I_sp(lower_ID(i)));
+    upper_rad = radial_data(I_sp(upper_ID(i)));
+
+    log_max = rad_max >= lower_rad & rad_max <= upper_rad;
+    log_min = rad_min >= lower_rad & rad_min <= upper_rad;
+
+    I_max_pks(log_max) = [];
+    max_pks(log_max) = [];
+    I_min_pks(log_min) = [];
+    min_pks(log_min) = [];
+end
+
+end
+
