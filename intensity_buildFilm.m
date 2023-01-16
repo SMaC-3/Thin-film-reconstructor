00743885ff119df417686e1f9183f392afeec1a7
Josh King <josh.king@monash.edu>
2023-01-16 19:29:10 +1100

    Implement update height reconstructor.

--- intensity_buildFilm.m
+++ intensity_buildFilm.m
@@ -1,29 +1,36 @@
+% Code written by Joshua P. King, SMaCLab, Monash University, Australia
+% Last updated: August, 2022
+
+%   ________  ___      ___       __       ______   ___            __       _______   
+%  /"       )|"  \    /"  |     /""\     /" _  "\ |"  |          /""\     |   _  "\  
+% (:   \___/  \   \  //   |    /    \   (: ( \___)||  |         /    \    (. |_)  :) 
+%  \___  \    /\\  \/.    |   /' /\  \   \/ \     |:  |        /' /\  \   |:     \/  
+%   __/  \\  |: \.        |  //  __'  \  //  \ _   \  |___    //  __'  \  (|  _  \\  
+%  /" \   :) |.  \    /:  | /   /  \\  \(:   _) \ ( \_|:  \  /   /  \\  \ |: |_)  :) 
+% (_______/  |___|\__/|___|(___/    \___)\_______) \_______)(___/    \___)(_______/  
+                                                                                   
+
+% SMaCLab website can be found here:
+% https://sites.google.com/view/smaclab
+
+% -------------------------------------------------------------
+
 function [T_dimp] =...
-    intensity_buildFilm(T_data, pre_T_dimp)
+    intensity_buildFilm(T_data, phi_correction, pre_T_dimp)
 
-%--------------------------------------------------------------------------
-% Smooth data
-%--------------------------------------------------------------------------
-
-% blue_int = smooth(blue_int);
-
-%--------------------------------------------------------------------------
+%--------------------------------------------------------------
 % Identify absolute height from red/blue intensity ratio with reference
 % to ideal, normalised curve
-%--------------------------------------------------------------------------
+%--------------------------------------------------------------
 
 [radius, dimp_h_red, dimp_h_blue, save_abs_h] =...
-    intensity_abs_h_ID(T_data, pre_T_dimp);
+    intensity_abs_h_ID_V2(T_data, phi_correction, pre_T_dimp);
 
 dimp_h_red = real(dimp_h_red);
 dimp_h_blue = real(dimp_h_blue);
 
 T_dimp = table(round(radius,4), round(dimp_h_red,4),round(dimp_h_blue,4),...
     'VariableNames',{'radius','red_film','blue_film'});
-
-% pixels_mm = 1792/2; 
-% Conversion from 2048 x 2048 image at 10x magnification -- need to divide by 2
-% pixels_um = pixels_mm/1000;
 
 figure(8)
 
